536513b12b30ec1becb7de4d79412c2543f8ab86
ayosimao <simao3175@gmail.com>
2025-11-22 22:22:08 +0000

    First Commit

---
+++ limpeza.js
@@ -0,0 +1,13 @@
+const ficheiro = "legislativas_raw.json";
+
+function cleanElectionData(rawData) {
+  const cleaned = { Inscritos: {}, Votantes: {} };
+  rawData.forEach((row) => {
+    const district = row["Círculo"];
+    if (district && district !== "Total") {
+      cleaned["Inscritos"][district] = parseInt(row["Inscritos"]);
+      cleaned["Votantes"][district] = parseInt(row["Votantes"]);
+    }
+  });
+  return cleaned;
+}
